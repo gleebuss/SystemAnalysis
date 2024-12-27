b4238884ebf2fd9e38d5339fe10d49c5614aa498
Gleb Agapov <glebagapov3@gmail.com>
2024-12-27 03:17:45 +0300

    fix task5

--- task5/task.py
+++ task5/task.py
@@ -1,7 +1,5 @@
-import argparse
 import json
 import numpy as np
-
 
 def get_matrix(str_json: str):
     clusters = [c if isinstance(c, list) else [c] for c in json.loads(str_json)]
@@ -34,7 +32,13 @@ def get_clusters(matrix):
     return str(final_result)
 
 
-def task(string1:str , string2:str):
+def main(file_a:str = "A.json" , file_b:str = "B.json"):
+    with open(file_a, "r") as f1:
+        string1 = f1.read().strip()
+
+    with open(file_b, "r") as f2:
+        string2 = f2.read().strip()
+
     matrix1 = get_matrix(string1)
     matrix2 = get_matrix(string2)
 
@@ -45,23 +49,5 @@ def task(string1:str , string2:str):
     clusters = get_clusters(matrix_or)
     return clusters
 
-def find_sublists(array_str):
-    array = json.loads(array_str)
-    sublists = [element for element in array if isinstance(element, list)]
-    return sublists
-
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file1", type=str)
-    parser.add_argument("file2", type=str)
-    args = parser.parse_args()
-    with open(args.file1, "r") as f1:
-        string1 = f1.read().strip()
-
-    with open(args.file2, "r") as f2:
-        string2 = f2.read().strip()
-
-    clusters = task(string1, string2)
-    results = find_sublists(clusters)
-    print(results)
+    print(main())
